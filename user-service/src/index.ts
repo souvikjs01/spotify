25a93114aaf82f8d8fa17432148089bcb76f3a7e
souvikjs01 <souvik741156@gmail.com>
2025-05-09 12:25:33 +0530

    complete backend

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -2,9 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes.js";
+import cors from "cors"
 
 dotenv.config()
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 connectDB()
 
